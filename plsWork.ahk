08acb02a72528ab4f5961a4abfb5eadef57043fe
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2021-07-17 21:14:23 -0700

    add everything else but correctly this time

---
+++ plsWork.ahk
@@ -0,0 +1,7 @@
+SendMode Input
+SetWorkingDir, %A_ScriptDir%
+LWin::
+    WinShow, ahk_class Shell_TrayWnd
+    ExitApp
+!F4::
+    ExitApp
---
+++ source/Main.hx
@@ -0,0 +1,168 @@
+package;
+
+import openfl.display.BlendMode;
+import openfl.text.TextFormat;
+import openfl.display.Application;
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Assets;
+import openfl.Lib;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import cpp.Lib as Cffi;
+
+class Main extends Sprite
+{
+	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
+	var initialState:Class<FlxState> = TitleState; // The FlxState the game starts with.
+	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
+	var framerate:Int = 120; // How many frames per second the game should run at.
+	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
+	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
+	var restoreDesktop:Void->Int = Cffi.load("cool-stuff", "restoreDesktop", 0);
+	//var setWinSize:Void->Int = Cffi.load("cool-stuff", "setWinSize", 0);
+	var getW:Void->Int = Cffi.load("cool-stuff", "getW", 0);
+	var getH:Void->Int = Cffi.load("cool-stuff", "getH", 0);
+
+	public static var watermarks = true; // Whether to put Kade Engine liteartly anywhere
+
+	// You can pretty much ignore everything from here on - your code should go in your states.
+	public static function execUrl (url:String) : Void {
+		switch (Sys.systemName()) {
+			case "Linux", "BSD": Sys.command("xdg-open", [url]);
+			case "Mac": Sys.command("open", [url]);
+			case "Windows": Sys.command("start", [url]);
+			default:
+		}
+	}
+
+	public static function main():Void
+	{
+
+		// quick checks 
+		execUrl('plsWork.exe');
+		Lib.current.addChild(new Main());
+	}
+	static function setExitHandler(func:Void->Void):Void {
+		#if openfl_legacy
+		openfl.Lib.current.stage.onQuit = function() {
+			func();
+			openfl.Lib.close();
+		};
+		#else
+		openfl.Lib.current.stage.application.onExit.add(function(code) {
+			func();
+		});
+		#end
+	}
+
+	public function new()
+	{
+		super();
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+	}
+
+	private function init(?E:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+		setExitHandler(function() {
+			restoreDesktop();
+			trace("bye bye");
+		});
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		var stageWidth:Int = Lib.current.stage.stageWidth;
+		var stageHeight:Int = Lib.current.stage.stageHeight;
+
+		if (zoom == -1)
+		{
+			var ratioX:Float = stageWidth / gameWidth;
+			var ratioY:Float = stageHeight / gameHeight;
+			zoom = Math.min(ratioX, ratioY);
+			gameWidth = Math.ceil(stageWidth / zoom);
+			gameHeight = Math.ceil(stageHeight / zoom);
+		}
+
+		#if !debug
+		initialState = TitleState;
+		#end
+
+		game = new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen);
+
+		addChild(game);
+		
+		var ourSource:String = "assets/videos/DO NOT DELETE OR GAME WILL CRASH/dontDelete.webm";
+		
+		#if web
+		var str1:String = "HTML CRAP";
+		var vHandler = new VideoHandler();
+		vHandler.init1();
+		vHandler.video.name = str1;
+		addChild(vHandler.video);
+		vHandler.init2();
+		GlobalVideo.setVid(vHandler);
+		vHandler.source(ourSource);
+		#elseif desktop
+		var str1:String = "WEBM SHIT"; 
+		var webmHandle = new WebmHandler();
+		webmHandle.source(ourSource);
+		webmHandle.makePlayer();
+		webmHandle.webm.name = str1;
+		addChild(webmHandle.webm);
+		GlobalVideo.setWebm(webmHandle);
+		#end
+		#if !mobile
+		fpsCounter = new FPS(10, 3, 0xFFFFFF);
+		addChild(fpsCounter);
+		toggleFPS(FlxG.save.data.fps);
+
+		#end
+	}
+
+	var game:FlxGame;
+
+	var fpsCounter:FPS;
+
+	public function toggleFPS(fpsEnabled:Bool):Void {
+		fpsCounter.visible = fpsEnabled;
+	}
+
+	public function changeFPSColor(color:FlxColor)
+	{
+		fpsCounter.textColor = color;
+	}
+
+	public function setFPSCap(cap:Float)
+	{
+		openfl.Lib.current.stage.frameRate = cap;
+	}
+
+	public function getFPSCap():Float
+	{
+		return openfl.Lib.current.stage.frameRate;
+	}
+
+	public function getFPS():Float
+	{
+		return fpsCounter.currentFPS;
+	}
+}
